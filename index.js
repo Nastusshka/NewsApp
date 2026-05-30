f673cf79dc7ff5cbd25c9bda3e948790f6d138bc
Nastusshka <achechushkina@gmail.com>
2026-05-30 11:12:12 +0500

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// Импортируем registerRootComponent, чтобы Expo правильно запустил приложение.
 import { registerRootComponent } from 'expo';
-
-// Импортируем главный компонент приложения.
 import App from './App';
-
-// Регистрируем приложение для запуска в Expo Go, на телефоне и в браузере.
 registerRootComponent(App);
